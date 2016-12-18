746b4bc753b99ef4837c0ef1417c6dde93f8a319
daiz <andrew@dispatch.ai>
2016-12-18 13:58:34 -0800

    - rpy topic (default off) - compensated imu data (default uncompensated) - binary serial port selection (default 2) - ned -> enu tf (default off) - add orientation qat to imu - expose vpe settings - calc rpy from quat (does not output both) - enable fluid_pressure - text mode does not have orientation

--- include/imu_vn_100/imu_vn_100.h
+++ include/imu_vn_100/imu_vn_100.h
@@ -117,12 +117,24 @@ class ImuVn100 {
   bool enable_mag_ = true;
   bool enable_pres_ = true;
   bool enable_temp_ = true;
+  bool enable_rpy_ = false;
+
   bool binary_output_ = true;
+  int binary_async_mode_ = BINARY_ASYNC_MODE_SERIAL_2;
+
+  bool imu_compensated_ = false;
+
+  bool tf_ned_to_enu_ = false;
+
+  bool vpe_enable_ = true;
+  int vpe_heading_mode_ = 1;
+  int vpe_filtering_mode_ = 1;
+  int vpe_tuning_mode_ = 1;
 
   SyncInfo sync_info_;
 
   du::Updater updater_;
-  DiagnosedPublisher pd_imu_, pd_mag_, pd_pres_, pd_temp_;
+  DiagnosedPublisher pd_imu_, pd_mag_, pd_pres_, pd_temp_, pd_rpy_;
 
   void FixImuRate();
   void LoadParameters();
